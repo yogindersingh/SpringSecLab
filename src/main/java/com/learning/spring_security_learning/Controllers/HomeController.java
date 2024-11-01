e484e49bb5529335e0ea1d9114c031339813d061
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2024-11-01 22:55:32 +0530

    added basic security with login and password

--- src/main/java/com/learning/spring_security_learning/Controllers/HomeController.java
+++ src/main/java/com/learning/spring_security_learning/Controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.learning.spring_security_learning.Controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HomeController {
 
   @GetMapping(path = "/home")
