81d08521b5d80ab75279728a80b2e388ab734f56
yogindersingh <yogindersingh@fico.com>
2024-11-02 20:58:51 +0530

    Basic controlles added and made the required url as secured or unsecured based on requirement

---
+++ src/main/java/com/learning/spring_security_learning/Controllers/AccountController.java
@@ -0,0 +1,16 @@
+package com.learning.spring_security_learning.Controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class AccountController {
+
+  @GetMapping(path = "/myAccount")
+  public String getAccountDetails(){
+    return "account details";
+  }
+
+}
