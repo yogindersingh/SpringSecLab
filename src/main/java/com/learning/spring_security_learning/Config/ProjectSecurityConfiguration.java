598f6690ae9525527ef150a3687a32ebd4ef5e0e
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2024-11-03 19:57:48 +0530

    more configurations added

--- src/main/java/com/learning/spring_security_learning/Config/ProjectSecurityConfiguration.java
+++ src/main/java/com/learning/spring_security_learning/Config/ProjectSecurityConfiguration.java
@@ -3,10 +3,17 @@ package com.learning.spring_security_learning.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfiguration {
@@ -16,7 +23,7 @@ public class ProjectSecurityConfiguration {
     http.authorizeHttpRequests((requests) -> {
       requests.requestMatchers("/myAccount", "myBalance", "myCards",
               "myLoans").authenticated().
-          requestMatchers("/contact", "/notices","/error").permitAll().
+          requestMatchers("/contact", "/notices", "/error").permitAll().
           requestMatchers("*/*").denyAll();
     });
     //If we want to disable UI form login or Basic credentials login
@@ -26,4 +33,28 @@ public class ProjectSecurityConfiguration {
     http.httpBasic(Customizer.withDefaults());
     return http.build();
   }
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    //here if we don't provide {noop} without defining any password encodespring security will throw password for
+    // password encoder
+//    UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("Admin").build();
+    UserDetails user= User.withUsername("user").password("{noop}user").roles("read").build();
+    UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$" +
+        ".v4IneOFgXYYzeEoMRorvOElgITLGTKk5AbmcvVGFngW26FMIctvi").roles("admin").build();
+    return new InMemoryUserDetailsManager(admin,user);
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+
+  @Bean
+  CompromisedPasswordChecker compromisedPasswordChecker() {
+    return new HaveIBeenPwnedRestApiPasswordChecker();
+  }
+
+
 }
