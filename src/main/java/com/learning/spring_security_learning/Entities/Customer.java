7d53707615e86d07af58a4430844e9f071078302
yogindersingh <yogindersingh@fico.com>
2025-02-18 19:30:46 +0530

    Changes to support JWT authentication

--- src/main/java/com/learning/spring_security_learning/Entities/Customer.java
+++ src/main/java/com/learning/spring_security_learning/Entities/Customer.java
@@ -3,6 +3,7 @@ package com.learning.spring_security_learning.Entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -38,11 +39,11 @@ public class Customer {
   @Column(name = "email")
   private String email;
 
-  @JsonIgnore
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @Column(name = "password")
   private String password;
 
-  @JsonIgnore
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   @JoinColumn(name = "customerId", nullable = false)
   private List<CustomerRoles> roles;
