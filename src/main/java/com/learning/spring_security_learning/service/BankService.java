94fb5ca1c443572ae6272e4080c5033dd7e22090
yogindersingh <38501528+yogindersingh@users.noreply.github.com>
2025-02-19 20:00:16 +0530

    Example for method filtering authorization

--- src/main/java/com/learning/spring_security_learning/service/BankService.java
+++ src/main/java/com/learning/spring_security_learning/service/BankService.java
@@ -1,7 +1,15 @@
 package com.learning.spring_security_learning.service;
 
+import com.learning.spring_security_learning.Controllers.LoginRequestDTO;
+import com.learning.spring_security_learning.Controllers.LoginResponseDTO;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BankService {
@@ -12,4 +20,13 @@ public class BankService {
   public String getResponseString() {
     return "get the balance";
   }
+
+
+  //Image for method filtering authorization
+//
+//  @PreFilter("filterObject.username!='firstname'")
+//  @PostFilter("filterObject.username!='firstname'")
+//  public List<LoginRequestDTO> saveData(List<LoginRequestDTO> data) {
+//    return data;
+//  }
 }
